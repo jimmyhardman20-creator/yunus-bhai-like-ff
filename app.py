db8b1761e5e755eb759b180ad85f5d80007cd79e
jimmyhardman20-creator <jimmyhardman20@gmail.com>
2025-10-31 16:15:13 +0600

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ async def send_request(encrypted_uid, token, url):
         'Expect': "100-continue",
         'X-Unity-Version': "2018.4.11f1",
         'X-GA': "v1 1",
-        'ReleaseVersion': "OB50"
+        'ReleaseVersion': "OB51"
     }
     async with aiohttp.ClientSession() as session:
         async with session.post(url, data=edata, headers=headers) as response:
@@ -150,7 +150,7 @@ def make_request(encrypt, server_name, token):
         'Expect': "100-continue",
         'X-Unity-Version': "2018.4.11f1",
         'X-GA': "v1 1",
-        'ReleaseVersion': "OB50"
+        'ReleaseVersion': "OB51"
     }
 
     response = requests.post(url, data=edata, headers=headers, verify=False)
